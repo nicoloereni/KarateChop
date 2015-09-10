e27ac1f49371e3f673d60beb4fd8b4b69accc791
nicolo <nicolo.ereni@trovaprezzi.it>
2015-09-10 15:03:58 +0200

    si va avanti

--- src/KarateChop.java
+++ src/KarateChop.java
@@ -3,23 +3,36 @@
 public class KarateChop {
     public int chop(int valueToFind, int[] arrayOfValues) {
 
+        int middlePosition = arrayOfValues.length / 2;
 
-        int middlePosition = arrayOfValues.length/2;
-
-        if(isArrayEmpty(arrayOfValues))
-        {
+        if (isArrayEmpty(arrayOfValues)) {
             return -1;
         }
-
-        if(arrayOfValues[middlePosition] == valueToFind){
-
+        if (arrayOfValues[middlePosition] == valueToFind) {
             return middlePosition;
+        } else if (isArrayGraterThanOne(arrayOfValues)) {
+            return getPositionWithValueFromArray(valueToFind, arrayOfValues);
         }
-        else
+        return -1;
+    }
+
+    private Integer getPositionWithValueFromArray(int valueToFind, int[] arrayOfValues) {
+        int middlePosition = arrayOfValues.length / 2;
+        if (isMiddlePositionValueGreaterThanValueToFind(arrayOfValues[middlePosition], valueToFind))
         {
             return chop(valueToFind, getArrayFromStartPositionToLastPosition(arrayOfValues, 0, middlePosition));
+        } else if (!isMiddlePositionValueGreaterThanValueToFind(arrayOfValues[middlePosition], valueToFind)) {
+            return chop(valueToFind, getArrayFromStartPositionToLastPosition(arrayOfValues, middlePosition, arrayOfValues.length)) + middlePosition;
         }
+        return null;
+    }
 
+    private boolean isMiddlePositionValueGreaterThanValueToFind(int middlePositionValue, int valueToFind) {
+        return middlePositionValue > valueToFind;
+    }
+
+    private boolean isArrayGraterThanOne(int[] arrayOfValues) {
+        return arrayOfValues.length >1;
     }
 
     private boolean isArrayEmpty(int[] arrayOfValues) {
@@ -27,14 +40,12 @@ public class KarateChop {
     }
 
     private int[] getArrayFromStartPositionToLastPosition(int[] arrayToCut, int startPosition, int endPosition) {
-
-        int[] result = new int[endPosition];
-
-        for(int index = startPosition; index<endPosition; index++){
-            result[index] = arrayToCut[index];
+        int[] result = new int[endPosition - startPosition];
+        int newIndex = 0;
+        for(int index = startPosition; index<endPosition; index++)
+        {
+            result[newIndex++] = arrayToCut[index];
         }
-
         return result;
     }
-
 }
--- src/KarateChopTest.java
+++ src/KarateChopTest.java
@@ -10,10 +10,10 @@ public class KarateChopTest{
         
         assertEquals(-1, karateChop.chop(3, new int[]{}));
         assertEquals(-1, karateChop.chop(3, new int[]{1}));
-        assertEquals(0,  karateChop.chop(1, new int[]{1}));
-        assertEquals(0,  karateChop.chop(1, new int[]{1, 3, 5}));
-        assertEquals(1,  karateChop.chop(3, new int[]{1, 3, 5}));
-        assertEquals(2,  karateChop.chop(5, new int[]{1, 3, 5}));
+//        assertEquals(0,  karateChop.chop(1, new int[]{1}));
+//        assertEquals(0,  karateChop.chop(1, new int[]{1, 3, 5}));
+//        assertEquals(1,  karateChop.chop(3, new int[]{1, 3, 5}));
+//        assertEquals(2,  karateChop.chop(5, new int[]{1, 3, 5}));
         /*assertEquals(-1, karateChop.chop(0, new int[]{1, 3, 5}));
         assertEquals(-1, karateChop.chop(2, new int[]{1, 3, 5}));
         assertEquals(-1, karateChop.chop(4, new int[]{1, 3, 5}));
